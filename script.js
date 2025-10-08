cb0f282d722aa22a03f050384ad512c67ac8b35f
Kristijan Maric <kiki.maric78@gmail.com>
2025-10-08 23:46:10 +0200

    Made function that populates the display with button click

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-function add (a, b) {
-   return (a + b )
+function add(a, b) {
+  return a + b;
 }
 
-function subtract (a, b) {
-    return (a - b)
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply (a, b) {
-    return (a * b)
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide (a ,b) {
-    return (a / b)
+function divide(a, b) {
+  return a / b;
 }
 
-function operate (operator, a , b) {
-    if (operator === "+") {
-        return add (a, b)
-    } else if (operator === "-") {
-        return subtract (a, b)
-    } else if (operator === "*") {
-        return multiply (a, b)
-    } else if (operator === "/") {
-        return divide (a, b)
-    } else return ("Invalid Operator")
+function operate(operator, a, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else return "Invalid Operator";
 }
 
-console.log(operate("*", 14, 2))
+function setupCalculator() {
+  const display = document.querySelector("#display");
+  const buttons = document.querySelectorAll("#buttons button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent = button.textContent
+    });
+  });
+}
+
+setupCalculator()
+
