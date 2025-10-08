79bc4a63800821587bb937af78dcbcfa49f7647b
Kristijan Maric <kiki.maric78@gmail.com>
2025-10-08 22:08:58 +0200

    Writen operate function
    Operate function takes one of the below weirtten functions for operators and 2 numbers

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-function addition (a, b) {
+function add (a, b) {
    return (a + b )
 }
 
-function subtraction (a, b) {
+function subtract (a, b) {
     return (a - b)
 }
 
-function multiplication (a, b) {
+function multiply (a, b) {
     return (a * b)
 }
 
-function division (a ,b) {
+function divide (a ,b) {
     return (a / b)
+}
+
+function operate (operator, a , b) {
+    if (operator === "+") {
+        return add (a, b)
+    } else if (operator === "-") {
+        return subtract (a, b)
+    } else if (operator === "*") {
+        return multiply (a, b)
+    } else if (operator === "/") {
+        return divide (a, b)
+    } else return ("Invalid Operator")
 }
